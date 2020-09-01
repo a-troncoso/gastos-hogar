c0bc3231d65c273405555707d0b33317dab8cab5
alvaro-troncoso <atroncoso.externo@coopeuch.cl>
2020-09-01 00:47:11 -0400

    adding rightarrow

--- src/domain/shared/Category.js
+++ src/domain/shared/Category.js
@@ -3,16 +3,43 @@ import PropTypes from "prop-types";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { toCurrencyFormat } from "../../utils/number";
 
-import color from '../../utils/styles/color'
+import { FontAwesome } from "@expo/vector-icons";
+import { SimpleLineIcons } from '@expo/vector-icons'; 
+
+import color from "../../utils/styles/color";
+
+const CategoryIcon = props => {
+  const { icon } = props;
+
+  return (
+    <View style={categoryIconStyles.categoryIcon}>
+      <FontAwesome name={icon} size={36} color={color.white} />
+    </View>
+  );
+};
+
+const categoryIconStyles = StyleSheet.create({
+  categoryIcon: {
+    height: "100%",
+    width: 72,
+    maxHeight: 72,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    backgroundColor: color.blue["0"]
+  }
+});
 
 const Category = props => {
   const { name, extraInfo, onPress } = props;
 
   return (
     <TouchableOpacity onPress={onPress} style={styles.category}>
+      <CategoryIcon icon="shopping-cart" />
       <View style={styles.categoryTextView}>
         <Text style={styles.categoryCategoryName}>{name}</Text>
       </View>
+      <SimpleLineIcons name="arrow-right" size={24} color="black" />
       {Object.keys(extraInfo).length > 0 && (
         <View style={styles.categoryExtraInfo}>
           <Text>Total este mes</Text>
@@ -25,10 +52,11 @@ const Category = props => {
 
 const styles = StyleSheet.create({
   category: {
-    height: 100,
-    backgroundColor: "#fff",
-    alignSelf: "stretch",
+    height: 96,
+    padding: 12,
     alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#fff",
     borderColor: color.blue["60"],
     borderStyle: "solid",
     borderWidth: 1,
@@ -40,9 +68,7 @@ const styles = StyleSheet.create({
   },
   categoryTextView: {
     flex: 1,
-    padding: 16,
-    alignSelf: 'flex-start',
-    justifyContent: "center"
+    padding: 16
   },
   categoryExtraInfo: {
     flex: 1,
